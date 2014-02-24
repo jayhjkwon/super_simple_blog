2c6487c4053079a478ce40da1cac951ac525837b
Jay H.J Kwon <nicekhj@gmail.com>
2014-02-24 16:59:21 +0900

    'fallback'

--- public/dist/templates.js
+++ public/dist/templates.js
@@ -1,15 +1,186 @@
-Ember.TEMPLATES["application"] = Ember.Handlebars.compile("<div class=\"navbar navbar-inverse navbar-fixed-top\">\r\n  <div class=\"navbar-inner\">\r\n    <div class=\"container-fluid\">\r\n      <button type=\"button\" class=\"btn btn-navbar\" data-toggle=\"collapse\" data-target=\".nav-collapse\">\r\n        <span class=\"icon-bar\"></span>\r\n        <span class=\"icon-bar\"></span>\r\n        <span class=\"icon-bar\"></span>\r\n      </button>\r\n      <a class=\"brand\" href=\"#\">Super Simple Blog</a>\r\n      <div class=\"nav-collapse collapse\">\r\n        <ul class=\"nav\">\r\n          <li>{{#link-to 'index'}}Home{{/link-to}}</li>\r\n          <li>{{#link-to 'posts'}}Posts{{/link-to}}</li>\r\n          <li>{{#link-to 'posts.new'}}New Post{{/link-to}}</li>\r\n        </ul>\r\n      </div>\r\n    </div>\r\n  </div>\r\n</div>\r\n{{outlet}}");
+Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, helper, options, self=this, helperMissing=helpers.helperMissing;
 
-Ember.TEMPLATES["index"] = Ember.Handlebars.compile("<div class=\"hero-unit\"><h1>Hello Ember.js</h1></div>");
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("Home");
+  }
 
-Ember.TEMPLATES["posts"] = Ember.Handlebars.compile("<div class=\"container-fluid\">\r\n  <div class=\"row-fluid\">\r\n    <div class=\"span4\">        \r\n      <table class=\"table\">\r\n        {{#each post in model}}\r\n          <tr><td>\r\n            {{#link-to 'posts.detail' post}}\r\n              {{post.title}}\r\n            {{/link-to}}\r\n            {{#link-to 'posts.edit' post class=\"btn pull-right\"}}Edit{{/link-to}}  \r\n            <button class=\"btn pull-right\" {{action 'deletePost' post}}>Delete</button>\r\n          </td></tr>\r\n        {{/each}}\r\n      </table>\r\n    </div>      \r\n    <div class=\"span8 well\">\r\n      {{outlet}}\r\n    </div>\r\n  </div>\r\n</div>");
+function program3(depth0,data) {
+  
+  
+  data.buffer.push("Posts");
+  }
 
-Ember.TEMPLATES["posts/_form"] = Ember.Handlebars.compile("<p><label for=\"title\">Title</label> {{input type=\"text\" value=title}}</p>\r\n<p><label for=\"contents\">Contents</label> {{textarea type=\"text\" value=contents}}</p>\r\n");
+function program5(depth0,data) {
+  
+  
+  data.buffer.push("New Post");
+  }
 
-Ember.TEMPLATES["posts/detail"] = Ember.Handlebars.compile("<h2>{{title}}</h2>\r\n<div>\r\n  {{markdown contents}}\r\n</div>\r\n<img {{bind-attr src=image_url}}></img>");
+  data.buffer.push("<div class=\"navbar navbar-inverse navbar-fixed-top\">\r\n  <div class=\"navbar-inner\">\r\n    <div class=\"container-fluid\">\r\n      <button type=\"button\" class=\"btn btn-navbar\" data-toggle=\"collapse\" data-target=\".nav-collapse\">\r\n        <span class=\"icon-bar\"></span>\r\n        <span class=\"icon-bar\"></span>\r\n        <span class=\"icon-bar\"></span>\r\n      </button>\r\n      <a class=\"brand\" href=\"#\">Super Simple Blog</a>\r\n      <div class=\"nav-collapse collapse\">\r\n        <ul class=\"nav\">\r\n          <li>");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</li>\r\n          <li>");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "posts", options) : helperMissing.call(depth0, "link-to", "posts", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</li>\r\n          <li>");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "posts.new", options) : helperMissing.call(depth0, "link-to", "posts.new", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</li>\r\n        </ul>\r\n      </div>\r\n    </div>\r\n  </div>\r\n</div>\r\n");
+  stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  return buffer;
+  
+});
 
-Ember.TEMPLATES["posts/edit"] = Ember.Handlebars.compile("{{partial 'posts/form'}}\r\n<button class=\"btn\" {{action 'editPost' this}}>Save</button>\r\n<h2>{{model.title}}</h2>\r\n<div>\r\n  {{markdown contents}}\r\n</div>  ");
+Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
 
-Ember.TEMPLATES["posts/index"] = Ember.Handlebars.compile("<div><h1>Select an article</h1></div>");
 
-Ember.TEMPLATES["posts/new"] = Ember.Handlebars.compile("{{partial 'posts/form'}}\r\n<button class=\"btn\" {{action 'addPost'}}>Save</button>\r\n<div class=\"well\">\r\n\t<h5>Preview</h5>\r\n\t<div>{{title}}</div>\r\n\t<div>{{contents}}</div>\r\n</div>");
+  data.buffer.push("<div class=\"hero-unit\"><h1>Hello Ember.js</h1></div>");
+  
+});
+
+Ember.TEMPLATES["posts"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+
+function program1(depth0,data) {
+  
+  var buffer = '', stack1, helper, options;
+  data.buffer.push("\r\n          <tr><td>\r\n            ");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[depth0,depth0],types:["STRING","ID"],data:data},helper ? helper.call(depth0, "posts.detail", "post", options) : helperMissing.call(depth0, "link-to", "posts.detail", "post", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\r\n            ");
+  stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{
+    'class': ("btn pull-right")
+  },hashTypes:{'class': "STRING"},hashContexts:{'class': depth0},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0,depth0],types:["STRING","ID"],data:data},helper ? helper.call(depth0, "posts.edit", "post", options) : helperMissing.call(depth0, "link-to", "posts.edit", "post", options));
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("  \r\n            <button class=\"btn pull-right\" ");
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "deletePost", "post", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0,depth0],types:["STRING","ID"],data:data})));
+  data.buffer.push(">Delete</button>\r\n          </td></tr>\r\n        ");
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\r\n              ");
+  stack1 = helpers._triageMustache.call(depth0, "post.title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\r\n            ");
+  return buffer;
+  }
+
+function program4(depth0,data) {
+  
+  
+  data.buffer.push("Edit");
+  }
+
+  data.buffer.push("<div class=\"container-fluid\">\r\n  <div class=\"row-fluid\">\r\n    <div class=\"span4\">        \r\n      <table class=\"table\">\r\n        ");
+  stack1 = helpers.each.call(depth0, "post", "in", "model", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\r\n      </table>\r\n    </div>      \r\n    <div class=\"span8 well\">\r\n      ");
+  stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\r\n    </div>\r\n  </div>\r\n</div>");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["posts/_form"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push("<p><label for=\"title\">Title</label> ");
+  data.buffer.push(escapeExpression((helper = helpers.input || (depth0 && depth0.input),options={hash:{
+    'type': ("text"),
+    'value': ("title")
+  },hashTypes:{'type': "STRING",'value': "ID"},hashContexts:{'type': depth0,'value': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "input", options))));
+  data.buffer.push("</p>\r\n<p><label for=\"contents\">Contents</label> ");
+  data.buffer.push(escapeExpression((helper = helpers.textarea || (depth0 && depth0.textarea),options={hash:{
+    'type': ("text"),
+    'value': ("contents")
+  },hashTypes:{'type': "STRING",'value': "ID"},hashContexts:{'type': depth0,'value': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "textarea", options))));
+  data.buffer.push("</p>\r\n");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["posts/detail"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push("<h2>");
+  stack1 = helpers._triageMustache.call(depth0, "title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</h2>\r\n<div>\r\n  ");
+  data.buffer.push(escapeExpression((helper = helpers.markdown || (depth0 && depth0.markdown),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "contents", options) : helperMissing.call(depth0, "markdown", "contents", options))));
+  data.buffer.push("\r\n</div>\r\n<img ");
+  data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
+    'src': ("image_url")
+  },hashTypes:{'src': "ID"},hashContexts:{'src': depth0},contexts:[],types:[],data:data})));
+  data.buffer.push("></img>");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["posts/edit"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push(escapeExpression((helper = helpers.partial || (depth0 && depth0.partial),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "posts/form", options) : helperMissing.call(depth0, "partial", "posts/form", options))));
+  data.buffer.push("\r\n<button class=\"btn\" ");
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "editPost", "", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0,depth0],types:["STRING","ID"],data:data})));
+  data.buffer.push(">Save</button>\r\n<h2>");
+  stack1 = helpers._triageMustache.call(depth0, "model.title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</h2>\r\n<div>\r\n  ");
+  data.buffer.push(escapeExpression((helper = helpers.markdown || (depth0 && depth0.markdown),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "contents", options) : helperMissing.call(depth0, "markdown", "contents", options))));
+  data.buffer.push("\r\n</div>  ");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["posts/index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<div><h1>Select an article</h1></div>");
+  
+});
+
+Ember.TEMPLATES["posts/new"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push(escapeExpression((helper = helpers.partial || (depth0 && depth0.partial),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "posts/form", options) : helperMissing.call(depth0, "partial", "posts/form", options))));
+  data.buffer.push("\r\n<button class=\"btn\" ");
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "addPost", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data})));
+  data.buffer.push(">Save</button>\r\n<div class=\"well\">\r\n	<h5>Preview</h5>\r\n	<div>");
+  stack1 = helpers._triageMustache.call(depth0, "title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</div>\r\n	<div>");
+  stack1 = helpers._triageMustache.call(depth0, "contents", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</div>\r\n</div>");
+  return buffer;
+  
+});
